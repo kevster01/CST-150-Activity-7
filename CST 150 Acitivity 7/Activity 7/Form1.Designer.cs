fa16f390dd4b6093aa122e7cec18aa6183d22d06
kevster01 <90598902+kevster01@users.noreply.github.com>
2022-01-30 08:05:42 -0800

    Add files via upload
    Assignment 7 package including images of working code

---
+++ CST 150 Acitivity 7/Activity 7/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Activity_7
+{
+    partial class ApproximatePi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ApproximatePi));
+            this.Terms_Lbl = new System.Windows.Forms.Label();
+            this.NumTerms_Text = new System.Windows.Forms.TextBox();
+            this.CalculateBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Terms_Lbl
+            // 
+            this.Terms_Lbl.AutoSize = true;
+            this.Terms_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Terms_Lbl.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Terms_Lbl.Location = new System.Drawing.Point(41, 87);
+            this.Terms_Lbl.Name = "Terms_Lbl";
+            this.Terms_Lbl.Size = new System.Drawing.Size(247, 32);
+            this.Terms_Lbl.TabIndex = 0;
+            this.Terms_Lbl.Text = "Enter # of Terms:";
+            // 
+            // NumTerms_Text
+            // 
+            this.NumTerms_Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumTerms_Text.Location = new System.Drawing.Point(422, 84);
+            this.NumTerms_Text.Name = "NumTerms_Text";
+            this.NumTerms_Text.Size = new System.Drawing.Size(219, 39);
+            this.NumTerms_Text.TabIndex = 1;
+            // 
+            // CalculateBtn
+            // 
+            this.CalculateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalculateBtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.CalculateBtn.Location = new System.Drawing.Point(47, 159);
+            this.CalculateBtn.Name = "CalculateBtn";
+            this.CalculateBtn.Size = new System.Drawing.Size(229, 50);
+            this.CalculateBtn.TabIndex = 2;
+            this.CalculateBtn.Text = "Calculate";
+            this.CalculateBtn.UseVisualStyleBackColor = true;
+            this.CalculateBtn.Click += new System.EventHandler(this.CalculateBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(47, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 29);
+            this.label1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(52, 373);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 29);
+            this.label3.TabIndex = 6;
+            // 
+            // ApproximatePi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CalculateBtn);
+            this.Controls.Add(this.NumTerms_Text);
+            this.Controls.Add(this.Terms_Lbl);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ApproximatePi";
+            this.Text = "Approximate Pi";
+            this.Load += new System.EventHandler(this.ApproximatePi_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Terms_Lbl;
+        private System.Windows.Forms.TextBox NumTerms_Text;
+        private System.Windows.Forms.Button CalculateBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CST 150 Acitivity 7/Activity 7/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_7
+{
+    public partial class ApproximatePi : Form
+    {
+        public ApproximatePi()
+        {
+            InitializeComponent();
+        }
+
+        private void ApproximatePi_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalculateBtn_Click(object sender, EventArgs e)
+        {
+            int n;
+            double piVal = 4, num = 4, den = 3;
+            n = Convert.ToInt32(NumTerms_Text.Text);
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    piVal -= num / den;
+                }
+                else
+                {
+                    piVal += num / den;
+                }
+                den += 2;
+            }
+
+            label1.Text = "Approximate value of pi after " + n + " terms";
+            label3.Text = "= " + piVal;
+      
+            
+            }
+        }
+}
+
